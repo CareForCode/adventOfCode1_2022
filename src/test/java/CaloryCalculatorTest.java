07138274833e021f1229f5b0cb64ae21cf1ceb99
patrickscheibert <patrick.scheibert@outlook.com>
2022-12-03 14:18:32 +0100

    solve part1

--- src/test/java/CaloryCalculatorTest.java
+++ src/test/java/CaloryCalculatorTest.java
@@ -3,6 +3,10 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -30,5 +34,35 @@ class CaloryCalculatorTest {
         int calories = inventory.getCalories();
 
         assertThat(calories).isEqualTo(expectedCalories);
+    }
+
+    @Test
+    void result1() {
+        BufferedReader reader;
+        int totalSum = 0;
+
+        List<Integer> caloryList = new ArrayList<>();
+        try {
+            reader = new BufferedReader(new FileReader("src/test/resources/adventOfCode1Input.txt"));
+            String line = reader.readLine();
+            List<Integer> inventory = new ArrayList<>();
+            while (line != null) {
+                if (line.equals("")) {
+                    caloryList.add(new Inventory(inventory).getCalories());
+                    inventory.clear();
+                } else {
+                    inventory.add(Integer.valueOf(line));
+                }
+                // read next line
+                line = reader.readLine();
+            }
+            caloryList.add(new Inventory(inventory).getCalories());
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        int highestCalories = caloryList.stream().mapToInt(value -> value).max().orElse(0);
+
+        assertThat(highestCalories).isEqualTo(68802);
     }
 }
